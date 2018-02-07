c8605d7cb6a30a19f6aab825c92ba4a889e24216
Vitaly-V <gtopbox@gmail.com>
2018-02-07 12:34:27 +0200

    Add notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,5 @@
-
 let deferredPrompt;
+const enableNotificationsButtons = document.querySelectorAll('.enable-notifications');
 
 if (!window.Promise) {
   window.Promise = Promise;
@@ -22,3 +22,37 @@ window.addEventListener('beforeinstallprompt', function(event) {
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotification() {
+  const options = {
+    body: 'You successfully subscribed through SW!',
+    icon: '/src/images/icons/app-icon-96x96.png',
+    image: '/src/images/sf-boat.jpg',
+    vibrate: [100, 50, 200],
+    badge: '/src/images/icons/app-icon-96x96.png',
+    tag: 'confirm-notification',
+    renotify: true,
+  };
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then(swr => swr.showNotification('Successfully subscribed!', options));
+  }
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(result => {
+    console.log('User choise', result);
+    if (result !== 'granted') {
+      console.log('No notification permission granted!');
+    } else {
+      displayConfirmNotification();
+    }
+  })
+}
+
+if ('Notification' in window) {
+  for (let i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = 'inline-block';
+    enableNotificationsButtons[i].addEventListener('click', askForNotificationPermission);
+  }
+}
